8f74c0bd1e6eee211390c5d8d5d43f1552b160bf
Chiril-00 <kskiril@gmail.com>
2022-07-28 19:22:33 +0300

    added code for bmi calculation part

--- curs16.js
+++ curs16.js
@@ -1,3 +1,29 @@
-document.querySelector('.verify').addEventListener('clcik', function(){
-    console.log(document.querySelector('.input').value);
+document.querySelector('.verify').addEventListener('click', function() {
+    let masa = document.querySelector('.input').value;;
+    let inaltimea = document.querySelector('.input1').value;;
+    let bmi;
+
+    bmi = masa / (inaltimea ** 2);
+    document.querySelector('.result').textContent = (`BMI este ${bmi}`);
 })
+
+
+
+
+// document.querySelector(".btn").addEventListener('click', function() {
+//     document.querySelector('.testclasa').textContent = "Acesta este un test";
+// })
+
+// 
+//     let text = ""
+//     let n = document.querySelector('.input').value;
+//     let nOriginal = n;
+//     let nInvers = "";
+//     while(n != 0) {
+//         nInvers = nInvers + (n % 10);
+//         n = (n - (n % 10)) / 10;
+//     }
+//     nOriginal == nInvers ? text = "Da, este palindrom" : text = "Nu, nu este palindrom";
+//     document.querySelector('.testclasa').textContent = text;
+
+// })
