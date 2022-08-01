ef9c3532a9a0a01308426a24f82831b0472086a0
Chiril-00 <kskiril@gmail.com>
2022-08-01 18:07:23 +0300

    JS changes

--- curs16.js
+++ curs16.js
@@ -1,8 +1,22 @@
-document.querySelector('.verify').addEventListener('click', function() {
-    let masa = document.querySelector('.input').value;;
-    let inaltimea = document.querySelector('.input1').value;;
-    let bmi;
+document.querySelector('.verify1').addEventListener('click', function() {
+    let masa1 = document.querySelector('.input11').value;;
+    let masa2 = document.querySelector('.input21').value;
+    let inaltimea1 = document.querySelector('.input12').value;;
+    let inaltimea2 = document.querySelector('.input22').value;
+    let name1 = document.querySelector('.name1').value;
+    let name2 = document.querySelector('.name2').value;
+    let bmi1;
+    let bmi2;
 
-    bmi = masa / (inaltimea ** 2);
-    document.querySelector('.result').textContent = (`BMI este ${bmi}`);
+    bmi1 = masa1 / (inaltimea1 ** 2);
+    bmi2 = masa2 / (inaltimea2 ** 2);
+    
+    document.querySelector('.result1').textContent = (`BMI lui ${name1} este ${bmi1}`);
+    document.querySelector('.result2').textContent = (`BMI lui ${name2} este ${bmi2}`);
+
+    if (bmi1 > bmi2) {
+            document.querySelector('.compare-result').textContent = (`${name1}  are BMI mai mare`);
+    } else {
+            document.querySelector('.compare-result').textContent = (`${name2}  are BMI mai mare`);
+    }
 })
